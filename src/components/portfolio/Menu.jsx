30901f38343514d2404b53f5d7c050da623d9b7a
Oratile <oratile.mabote@dadevs.co.za>
2022-11-30 08:29:56 +0200

    df

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -12,43 +12,68 @@ const Menu = [
         image: Work1,
         title: "Task manager Application",
         description: "All the Intotek employees used this application to manage their day to day tasks",
+        technologies: <ol>
+            <ul>PHP</ul>
+            <ul>MySQL</ul>
+            <ul>Xampp</ul>
+        </ol>,
         category: "Intotek",
     },
-    {
-        id: 6,
-        image: Work6,
-        title: "Alumni App",
-        description: "Companies track the employment status of thier former interns(Alumni",
-        category: "DA",
-    },
+    // {
+    //     id: 6,
+    //     image: Work6,
+    //     title: "Alumni App",
+    //     description: "Companies track the employment status of thier former interns(Alumni",
+    //     technologies: <ol>
+    //     <ul>PHP</ul>
+    //     <ul>MySQL</ul>
+    //     <ul>Xampp</ul>
+    // </ol>,
+    //     category: "DA",
+    // },
    
     {
         id: 3,
         image: Work3,
         title: "School Register and Demerit System",
         description:"High School learners used the app to mark register daily based on thier location",
+        technologies: <ol>
+        <ul>PHP</ul>
+        <ul>MySQL</ul>
+        <ul>Xampp</ul>
+    </ol>,
         category: "University",
     },
-    {
-        id: 5,
-        image: Work5,
-        title: "To-do app",
-        description:"Users manage thier daily to daily task by inputing them on the app and change status when done",
-        category: "DA",
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: "Social Media clone",
-        description: "Social media platform where users can blog,follow/unfollow and like post of others",
-        category: "DA",
-    },
+    // {
+    //     id: 5,
+    //     image: Work5,
+    //     title: "To-do app",
+    //     description:"Users manage thier daily to daily task by inputing them on the app and change status when done",
+    //     technologies: <ol>
+    //     <ul>PHP</ul>
+    //     <ul>MySQL</ul>
+    //     <ul>Xampp</ul>
+    // </ol>,
+    //     category: "DA",
+    // },
+    // {
+    //     id: 4,
+    //     image: Work4,
+    //     title: "Social Media clone",
+    //     description: "Social media platform where users can blog,follow/unfollow and like post of others",
+    //     category: "DA",
+    // },
     
     {
         id: 2,
         image: Work2,
         title: "Portfolio website",
         description:"My own resume where I showcase my skills and experience",
+        technologies: <ol>
+        <ul>PHP</ul>
+        <ul>MySQL</ul>
+        <ul>Xampp</ul>
+    </ol>,
         category: "DA",
     },
 
